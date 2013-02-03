dd1c722794cd80427b8d66bda53e1bc110ea784a
Hayato Yokota <yokochie@gmail.com>
2013-02-04 01:27:01 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmybbs::Application.config.secret_token = 'f7839044d7fb15df9b2e9504352b14d576d3e3fbca054e233d00a2a3b98e58a17012d22218d21d8547e6891e02889e3403693d66b46d8aafda8d12d7a944d859'
