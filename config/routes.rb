318bc08eac207029336d052745de1e9c4d0a9a3f
Hayato Yokota <yokochie@gmail.com>
2013-02-04 19:46:32 +0900

    fix root

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Mmybbs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
